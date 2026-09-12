48d6c7eee1c194b80da35b7d759087a93fdbbaed
joecar61 <joecar@outlook.com>
2026-09-11 19:42:35 -0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,1 @@
+const CACHE="rcc-scorecard-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
